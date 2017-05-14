d653319d434de4aa332ecd6bf3e5c1ae00cc6555
Kyle Amoroso <kamoroso1994@gmail.com>
2017-05-14 10:05:56 -0400

    Changed tabs to spaces

--- main.js
+++ main.js
@@ -9,175 +9,175 @@ const TPS = 60;
 window.addEventListener("load", init);
 
 function init() {
-	canvas = document.querySelector("canvas");
-	ctx = canvas.getContext("2d");
+    canvas = document.querySelector("canvas");
+    ctx = canvas.getContext("2d");
 
-	qt = new QuadTree(0, 0, canvas.width, canvas.height);
-	particles = [];
+    qt = new QuadTree(0, 0, canvas.width, canvas.height);
+    particles = [];
 
-	canvas.addEventListener("click", (event) => {
-		const bcr = canvas.getBoundingClientRect();
-		const angle = 2 * Math.PI * Math.random();
-		const p = {
-			x: event.clientX - bcr.left,
-			y: event.clientY - bcr.top,
-			vx: 100 * Math.cos(angle),
-			vy: 100 * Math.sin(angle)
-		};
+    canvas.addEventListener("click", (event) => {
+        const bcr = canvas.getBoundingClientRect();
+        const angle = 2 * Math.PI * Math.random();
+        const p = {
+            x: event.clientX - bcr.left,
+            y: event.clientY - bcr.top,
+            vx: 100 * Math.cos(angle),
+            vy: 100 * Math.sin(angle)
+        };
 
-		if(!qt.put({x: p.x, y: p.y}, p)) {
-			console.log("Failed to put", p);
+        if(!qt.put({x: p.x, y: p.y}, p)) {
+            console.log("Failed to put", p);
 
-			if(particles.indexOf(p) < 0) {
-				console.log("Not in particles");
-			}
-		} else {
-			particles.push(p);
-		}
-	});
+            if(particles.indexOf(p) < 0) {
+                console.log("Not in particles");
+            }
+        } else {
+            particles.push(p);
+        }
+    });
 
-	document.getElementById("explode").addEventListener("click", () => {
-		const boomRadius = RADIUS / Math.sin(Math.PI / BOOM_COUNT);
+    document.getElementById("explode").addEventListener("click", () => {
+        const boomRadius = RADIUS / Math.sin(Math.PI / BOOM_COUNT);
 
-		for(let i = 0; i < BOOM_COUNT && particles.length < MAX_PARTICLES; i++) {
-			const angle = i / BOOM_COUNT * 2 * Math.PI;
-			const sin = Math.sin(angle);
-			const cos = Math.cos(angle);
+        for(let i = 0; i < BOOM_COUNT && particles.length < MAX_PARTICLES; i++) {
+            const angle = i / BOOM_COUNT * 2 * Math.PI;
+            const sin = Math.sin(angle);
+            const cos = Math.cos(angle);
 
-			const p = {
-				x: canvas.width / 2 + boomRadius * cos,
-				y: canvas.height / 2 + boomRadius * sin,
-				vx: 200 * cos,
-				vy: 200 * sin
-			};
+            const p = {
+                x: canvas.width / 2 + boomRadius * cos,
+                y: canvas.height / 2 + boomRadius * sin,
+                vx: 200 * cos,
+                vy: 200 * sin
+            };
 
-			if(!qt.put({x: p.x, y: p.y}, p)) {
-				console.log("Failed to put", p);
+            if(!qt.put({x: p.x, y: p.y}, p)) {
+                console.log("Failed to put", p);
 
-				if(particles.indexOf(p) < 0) {
-					console.log("Not in particles");
-				}
-			} else {
-				particles.push(p);
-			}
-		}
-	});
+                if(particles.indexOf(p) < 0) {
+                    console.log("Not in particles");
+                }
+            } else {
+                particles.push(p);
+            }
+        }
+    });
 
-	const toggleBtn = document.getElementById("toggle");
-	toggleBtn.addEventListener("click", () => {
-		if(canvas.hasAttribute("data-paused")) {
-			canvas.removeAttribute("data-paused");
-			toggleBtn.textContent = "Pause";
-			resume();
-		} else {
-			canvas.setAttribute("data-paused", true);
-			toggleBtn.textContent = "Play";
-			pause();
-		}
-	});
+    const toggleBtn = document.getElementById("toggle");
+    toggleBtn.addEventListener("click", () => {
+        if(canvas.hasAttribute("data-paused")) {
+            canvas.removeAttribute("data-paused");
+            toggleBtn.textContent = "Pause";
+            resume();
+        } else {
+            canvas.setAttribute("data-paused", true);
+            toggleBtn.textContent = "Play";
+            pause();
+        }
+    });
 
-	document.getElementById("clear").addEventListener("click", () => {
-		qt.clear();
-		particles = [];
-	});
+    document.getElementById("clear").addEventListener("click", () => {
+        qt.clear();
+        particles = [];
+    });
 
-	lastDraw = Date.now();
-	drawId = requestAnimationFrame(draw);
-	lastTick = Date.now();
-	tickId = setTimeout(tick, 1000 / TPS);
+    lastDraw = Date.now();
+    drawId = requestAnimationFrame(draw);
+    lastTick = Date.now();
+    tickId = setTimeout(tick, 1000 / TPS);
 }
 
 function draw() {
-	const currentDraw = Date.now();
+    const currentDraw = Date.now();
 
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	particles.forEach(function(p1) {
-		const neighbors = qt.getAll(p1.x - 2 * RADIUS, p1.y - 2 * RADIUS, 4 * RADIUS, 4 * RADIUS);
+    particles.forEach(function(p1) {
+        const neighbors = qt.getAll(p1.x - 2 * RADIUS, p1.y - 2 * RADIUS, 4 * RADIUS, 4 * RADIUS);
 
-		ctx.fillStyle = "#0000ff";
+        ctx.fillStyle = "#0000ff";
 
-		if(neighbors.some(p2 => {
-			if(p1 != p2) {
-				const dx = p2.x - p1.x;
-				const dy = p2.y - p1.y;
+        if(neighbors.some(p2 => {
+            if(p1 != p2) {
+                const dx = p2.x - p1.x;
+                const dy = p2.y - p1.y;
 
-				if(dx * dx + dy * dy < 4 * RADIUS * RADIUS) {
-					return true;
-				}
-			}
-			return false;
-		})) {
-			ctx.fillStyle = "#ff0000";
-		}
+                if(dx * dx + dy * dy < 4 * RADIUS * RADIUS) {
+                    return true;
+                }
+            }
+            return false;
+        })) {
+            ctx.fillStyle = "#ff0000";
+        }
 
-		ctx.beginPath();
-		ctx.arc(p1.x, p1.y, RADIUS, 0, 2 * Math.PI);
-		ctx.fill();
-	});
+        ctx.beginPath();
+        ctx.arc(p1.x, p1.y, RADIUS, 0, 2 * Math.PI);
+        ctx.fill();
+    });
 
-	ctx.strokeStyle = "#00ff00";
-	qt.draw(ctx);
+    ctx.strokeStyle = "#00ff00";
+    qt.draw(ctx);
 
-	lastDraw = currentDraw;
-	drawId = requestAnimationFrame(draw);
+    lastDraw = currentDraw;
+    drawId = requestAnimationFrame(draw);
 }
 
 function tick() {
-	const currentTick = Date.now();
-	const dt = (currentTick - lastTick) / 1000;
+    const currentTick = Date.now();
+    const dt = (currentTick - lastTick) / 1000;
 
-	qt.clear();
-	for(let i = 0; i < particles.length; i++) {
-		const p = particles[i];
+    qt.clear();
+    for(let i = 0; i < particles.length; i++) {
+        const p = particles[i];
 
-		p.x += p.vx * dt;
-		p.y += p.vy * dt;
+        p.x += p.vx * dt;
+        p.y += p.vy * dt;
 
-		if(p.x - RADIUS < 0) {
-			p.x = RADIUS;
-			p.vx *= -1;
-		}
-		if(p.x + RADIUS >= canvas.width) {
-			p.x = canvas.width - RADIUS - 1;
-			p.vx *= -1;
-		}
-		if(p.y - RADIUS < 0) {
-			p.y = RADIUS;
-			p.vy *= -1;
-		}
-		if(p.y + RADIUS >= canvas.height) {
-			p.y = canvas.height - RADIUS - 1;
-			p.vy *= -1;
-		}
+        if(p.x - RADIUS < 0) {
+            p.x = RADIUS;
+            p.vx *= -1;
+        }
+        if(p.x + RADIUS >= canvas.width) {
+            p.x = canvas.width - RADIUS - 1;
+            p.vx *= -1;
+        }
+        if(p.y - RADIUS < 0) {
+            p.y = RADIUS;
+            p.vy *= -1;
+        }
+        if(p.y + RADIUS >= canvas.height) {
+            p.y = canvas.height - RADIUS - 1;
+            p.vy *= -1;
+        }
 
-		if(!qt.put({x: p.x, y: p.y}, p)) {
-			console.log("Failed to put", p);
+        if(!qt.put({x: p.x, y: p.y}, p)) {
+            console.log("Failed to put", p);
 
-			particles.splice(i, 1);
-			i--;
-		}
+            particles.splice(i, 1);
+            i--;
+        }
 
-		if(qt.get({x: p.x, y: p.y}) == null) {
-			console.log("Failed to put", p);
-			return;
-		}
-	}
+        if(qt.get({x: p.x, y: p.y}) == null) {
+            console.log("Failed to put", p);
+            return;
+        }
+    }
 
-	document.getElementById("particles").textContent = particles.length;
+    document.getElementById("particles").textContent = particles.length;
 
-	lastTick = currentTick;
-	tickId = setTimeout(tick, 1000 / TPS);
+    lastTick = currentTick;
+    tickId = setTimeout(tick, 1000 / TPS);
 }
 
 function resume() {
-	lastDraw = Date.now();
-	drawId = requestAnimationFrame(draw);
-	lastTick = Date.now();
-	tickId = setTimeout(tick, 1000 / TPS);
+    lastDraw = Date.now();
+    drawId = requestAnimationFrame(draw);
+    lastTick = Date.now();
+    tickId = setTimeout(tick, 1000 / TPS);
 }
 
 function pause() {
-	cancelAnimationFrame(drawId);
-	clearTimeout(tickId);
+    cancelAnimationFrame(drawId);
+    clearTimeout(tickId);
 }
--- quadtree.js
+++ quadtree.js
@@ -1,312 +1,312 @@
 (function(window) {
-	"use strict";
-
-	const Point = {
-		equals(p1, p2) {
-			return p1 == p2 || p1.x == p2.x && p1.y == p2.y;
-		}
-	};
-
-	class AABB {
-		constructor(x, y, w, h) {
-			if(w < 0) {
-				x += w;
-				w *= -1;
-			}
-			if(h < 0) {
-				y += h;
-				h *= -1;
-			}
-
-			this.x = x;
-			this.y = y;
-			this.left = x;
-			this.top = y;
-			this.right = x + w;
-			this.bottom = y + h;
-			this.width = w;
-			this.height = h;
-		}
-
-		equals(aabb) {
-			return this == aabb || aabb != null && this.x == aabb.x && this.y == aabb.y && this.width == aabb.width && this.height == aabb.height;
-		}
-
-		contains(point) {
-			return point != null && point.x >= this.left && point.x < this.right && point.y >= this.top && point.y < this.bottom;
-		}
-
-		// "Fast rectangle to rectangle intersection" http://stackoverflow.com/a/2752369/2727710
-		intersects(aabb) {
-			return this == aabb || aabb != null && this.left < aabb.right && aabb.left < this.right && this.top < aabb.bottom && aabb.top < this.bottom;
-		}
-	}
-
-	class QuadTree {
-		constructor(x, y, w, h) {
-			this.aabb = new AABB(x, y, w, h);
-			this.root = new Node(this.aabb);
-			this.entryCount = 0;
-		}
-
-		get(key) {
-			return this.isKeyValid(key) ? this.root.get(key) : null;
-		}
-
-		getAll(x, y, w, h) {
-			return this.root.getAll(new AABB(x, y, w, h));
-		}
-
-		put(key, value) {
-			const success = this.isKeyValid(key) && this.isValueValid(value) && this.root.put(key, value);
-
-			if(success) {
-				this.entryCount++;
-			}
-
-			return success;
-		}
-
-		remove(key) {
-			const removedValue = this.isKeyValid(key) ? this.root.remove(key) : null;
-
-			if(removedValue != null) {
-				this.entryCount--;
-			}
-
-			return removedValue;
-		}
-
-		size() {
-			return this.entryCount;
-		}
-
-		getHeight() {
-			return this.root.getHeight();
-		}
-
-		isEmpty() {
-			return this.entryCount == 0;
-		}
-
-		isKeyValid(key) {
-			return this.aabb.contains(key);
-		}
-
-		isValueValid(value) {
-			return value != null;
-		}
-
-		containsKey(key) {
-			return this.isKeyValid(key) && this.root.containsKey(key);
-		}
-
-		containsValue(value) {
-			return this.isValueValid(value) && this.root.containsValue(value);
-		}
-
-		clear() {
-			this.root = new Node(this.aabb);
-			this.entryCount = 0;
-		}
-
-		draw(ctx) {
-			this.root.draw(ctx);
-		}
-	}
-
-	class Node {
-		constructor(aabb) {
-			this.aabb = aabb;
-			this.children = [null, null, null, null];
-			this.keys = [];
-			this.values = [];
-		}
-
-		getChildIndex(key) {
-			const bitLeft = key.x < this.aabb.x + this.aabb.width / 2 ? 0 : 1;
-			const bitTop = key.y < this.aabb.y + this.aabb.height / 2 ? 0 : 1;
-
-			return bitLeft << 1 | bitTop;
-		}
-
-		getKeyIndex(key1) {
-			return this.keys.findIndex(key2 => Point.equals(key1, key2));
-		}
-
-		findChild(key) {
-			const index = this.getChildIndex(key);
-			let child = this.children[index];
-
-			if(child == null) {
-				const childAABB = new AABB(
-					this.aabb.x + (index >> 1 & 1) * this.aabb.width / 2,
-					this.aabb.y + (index & 1) * this.aabb.height / 2,
-					this.aabb.width / 2,
-					this.aabb.height / 2
-				);
-				child = new Node(childAABB);
-
-				this.children[index] = child;
-			}
-
-			return child;
-		}
-
-		get(key) {
-			if(!this.isLeaf()) {
-				const child = this.children[this.getChildIndex(key)];
-
-				if(child == null) {
-					return null;
-				}
-
-				return child.get(key);
-			}
-
-			const index = this.getKeyIndex(key);
-
-			return index >= 0 ? this.values[index] : null;
-		}
-
-		getAll(aabb) {
-			const all = [];
-
-			if(!this.isLeaf()) {
-				// collect results from getAll invoked on eligible children
-				this.children.forEach(child => {
-					if(child != null && aabb.intersects(child.aabb)) {
-						all.push(...child.getAll(aabb));
-					}
-				});
-			} else {
-				// collect values that lie in AABB
-				this.keys.forEach((key, index) => {
-					if(aabb.contains(key)) {
-						all.push(this.values[index]);
-					}
-				});
-			}
-
-			return all;
-		}
-
-		put(key, value) {
-			if(!this.isLeaf()) {
-				return this.findChild(key).put(key, value);
-			}
-
-			if(this.getKeyIndex(key) >= 0) {
-				return false;
-			}
-
-			this.keys.push(key);
-			this.values.push(value);
-			this.split();
-
-			return true;
-		}
-
-		remove(key) {
-			if(!this.isLeaf()) {
-				const index = this.getChildIndex(key);
-				const child = this.children[index];
-
-				if(child == null) {
-					return null;
-				}
-
-				const removedValue = child.remove(key);
-
-				if(child.isEmpty()) {
-					this.children[index] = null;
-				}
-
-				return removedValue;
-			}
-
-			const index = this.getKeyIndex(key);
-
-			if(index >= 0) {
-				const removedValue = this.values[index];
-				this.keys.splice(index, 1);
-				this.values.splice(index, 1);
-				return removedValue;
-			}
-
-			return null;
-		}
-
-		getHeight() {
-			let height = 0;
-
-			this.children.forEach(child => {
-				if(child != null) {
-					height = Math.max(height, child.getHeight());
-				}
-			});
-
-			return 1 + height;
-		}
-
-		isEmpty() {
-			return this.isLeaf() && this.keys.length == 0;
-		}
-
-		containsKey(key) {
-			if(!this.isLeaf()) {
-				const child = this.children[this.getChildIndex(key)];
-
-				return child != null && child.containsKey(key);
-			}
-
-			return this.getKeyIndex(key) >= 0;
-		}
-
-		containsValue(value) {
-			if(!this.isLeaf()) {
-				return this.children.some(child => child != null && child.containsValue(value));
-			}
-
-			return this.values.includes(value);
-		}
-
-		draw(ctx) {
-			this.children.forEach(child => {
-				if(child != null) {
-					child.draw(ctx);
-				}
-			});
-
-			ctx.strokeRect(this.aabb.x, this.aabb.y, this.aabb.width, this.aabb.height);
-		}
-
-		isLeaf() {
-			return this.children.every(child => child == null);
-		}
-
-		split() {
-			if(this.keys.length <= Node.MAX_BUCKET_SIZE) {
-				return;
-			}
-
-			while(this.keys.length > 0) {
-				var key = this.keys.pop();
-				var value = this.values.pop();
-				var child = this.findChild(key);
-
-				child.keys.push(key);
-				child.values.push(value);
-			}
-
-			this.children.forEach(child => {
-				if(child != null) {
-					child.split();
-				}
-			});
-		}
-	}
-	Node.MAX_BUCKET_SIZE = 5;
-
-	window.QuadTree = QuadTree;
+    "use strict";
+
+    const Point = {
+        equals(p1, p2) {
+            return p1 == p2 || p1.x == p2.x && p1.y == p2.y;
+        }
+    };
+
+    class AABB {
+        constructor(x, y, w, h) {
+            if(w < 0) {
+                x += w;
+                w *= -1;
+            }
+            if(h < 0) {
+                y += h;
+                h *= -1;
+            }
+
+            this.x = x;
+            this.y = y;
+            this.left = x;
+            this.top = y;
+            this.right = x + w;
+            this.bottom = y + h;
+            this.width = w;
+            this.height = h;
+        }
+
+        equals(aabb) {
+            return this == aabb || aabb != null && this.x == aabb.x && this.y == aabb.y && this.width == aabb.width && this.height == aabb.height;
+        }
+
+        contains(point) {
+            return point != null && point.x >= this.left && point.x < this.right && point.y >= this.top && point.y < this.bottom;
+        }
+
+        // "Fast rectangle to rectangle intersection" http://stackoverflow.com/a/2752369/2727710
+        intersects(aabb) {
+            return this == aabb || aabb != null && this.left < aabb.right && aabb.left < this.right && this.top < aabb.bottom && aabb.top < this.bottom;
+        }
+    }
+
+    class QuadTree {
+        constructor(x, y, w, h) {
+            this.aabb = new AABB(x, y, w, h);
+            this.root = new Node(this.aabb);
+            this.entryCount = 0;
+        }
+
+        get(key) {
+            return this.isKeyValid(key) ? this.root.get(key) : null;
+        }
+
+        getAll(x, y, w, h) {
+            return this.root.getAll(new AABB(x, y, w, h));
+        }
+
+        put(key, value) {
+            const success = this.isKeyValid(key) && this.isValueValid(value) && this.root.put(key, value);
+
+            if(success) {
+                this.entryCount++;
+            }
+
+            return success;
+        }
+
+        remove(key) {
+            const removedValue = this.isKeyValid(key) ? this.root.remove(key) : null;
+
+            if(removedValue != null) {
+                this.entryCount--;
+            }
+
+            return removedValue;
+        }
+
+        size() {
+            return this.entryCount;
+        }
+
+        getHeight() {
+            return this.root.getHeight();
+        }
+
+        isEmpty() {
+            return this.entryCount == 0;
+        }
+
+        isKeyValid(key) {
+            return this.aabb.contains(key);
+        }
+
+        isValueValid(value) {
+            return value != null;
+        }
+
+        containsKey(key) {
+            return this.isKeyValid(key) && this.root.containsKey(key);
+        }
+
+        containsValue(value) {
+            return this.isValueValid(value) && this.root.containsValue(value);
+        }
+
+        clear() {
+            this.root = new Node(this.aabb);
+            this.entryCount = 0;
+        }
+
+        draw(ctx) {
+            this.root.draw(ctx);
+        }
+    }
+
+    class Node {
+        constructor(aabb) {
+            this.aabb = aabb;
+            this.children = [null, null, null, null];
+            this.keys = [];
+            this.values = [];
+        }
+
+        getChildIndex(key) {
+            const bitLeft = key.x < this.aabb.x + this.aabb.width / 2 ? 0 : 1;
+            const bitTop = key.y < this.aabb.y + this.aabb.height / 2 ? 0 : 1;
+
+            return bitLeft << 1 | bitTop;
+        }
+
+        getKeyIndex(key1) {
+            return this.keys.findIndex(key2 => Point.equals(key1, key2));
+        }
+
+        findChild(key) {
+            const index = this.getChildIndex(key);
+            let child = this.children[index];
+
+            if(child == null) {
+                const childAABB = new AABB(
+                    this.aabb.x + (index >> 1 & 1) * this.aabb.width / 2,
+                    this.aabb.y + (index & 1) * this.aabb.height / 2,
+                    this.aabb.width / 2,
+                    this.aabb.height / 2
+                );
+                child = new Node(childAABB);
+
+                this.children[index] = child;
+            }
+
+            return child;
+        }
+
+        get(key) {
+            if(!this.isLeaf()) {
+                const child = this.children[this.getChildIndex(key)];
+
+                if(child == null) {
+                    return null;
+                }
+
+                return child.get(key);
+            }
+
+            const index = this.getKeyIndex(key);
+
+            return index >= 0 ? this.values[index] : null;
+        }
+
+        getAll(aabb) {
+            const all = [];
+
+            if(!this.isLeaf()) {
+                // collect results from getAll invoked on eligible children
+                this.children.forEach(child => {
+                    if(child != null && aabb.intersects(child.aabb)) {
+                        all.push(...child.getAll(aabb));
+                    }
+                });
+            } else {
+                // collect values that lie in AABB
+                this.keys.forEach((key, index) => {
+                    if(aabb.contains(key)) {
+                        all.push(this.values[index]);
+                    }
+                });
+            }
+
+            return all;
+        }
+
+        put(key, value) {
+            if(!this.isLeaf()) {
+                return this.findChild(key).put(key, value);
+            }
+
+            if(this.getKeyIndex(key) >= 0) {
+                return false;
+            }
+
+            this.keys.push(key);
+            this.values.push(value);
+            this.split();
+
+            return true;
+        }
+
+        remove(key) {
+            if(!this.isLeaf()) {
+                const index = this.getChildIndex(key);
+                const child = this.children[index];
+
+                if(child == null) {
+                    return null;
+                }
+
+                const removedValue = child.remove(key);
+
+                if(child.isEmpty()) {
+                    this.children[index] = null;
+                }
+
+                return removedValue;
+            }
+
+            const index = this.getKeyIndex(key);
+
+            if(index >= 0) {
+                const removedValue = this.values[index];
+                this.keys.splice(index, 1);
+                this.values.splice(index, 1);
+                return removedValue;
+            }
+
+            return null;
+        }
+
+        getHeight() {
+            let height = 0;
+
+            this.children.forEach(child => {
+                if(child != null) {
+                    height = Math.max(height, child.getHeight());
+                }
+            });
+
+            return 1 + height;
+        }
+
+        isEmpty() {
+            return this.isLeaf() && this.keys.length == 0;
+        }
+
+        containsKey(key) {
+            if(!this.isLeaf()) {
+                const child = this.children[this.getChildIndex(key)];
+
+                return child != null && child.containsKey(key);
+            }
+
+            return this.getKeyIndex(key) >= 0;
+        }
+
+        containsValue(value) {
+            if(!this.isLeaf()) {
+                return this.children.some(child => child != null && child.containsValue(value));
+            }
+
+            return this.values.includes(value);
+        }
+
+        draw(ctx) {
+            this.children.forEach(child => {
+                if(child != null) {
+                    child.draw(ctx);
+                }
+            });
+
+            ctx.strokeRect(this.aabb.x, this.aabb.y, this.aabb.width, this.aabb.height);
+        }
+
+        isLeaf() {
+            return this.children.every(child => child == null);
+        }
+
+        split() {
+            if(this.keys.length <= Node.MAX_BUCKET_SIZE) {
+                return;
+            }
+
+            while(this.keys.length > 0) {
+                var key = this.keys.pop();
+                var value = this.values.pop();
+                var child = this.findChild(key);
+
+                child.keys.push(key);
+                child.values.push(value);
+            }
+
+            this.children.forEach(child => {
+                if(child != null) {
+                    child.split();
+                }
+            });
+        }
+    }
+    Node.MAX_BUCKET_SIZE = 5;
+
+    window.QuadTree = QuadTree;
 })(window);
